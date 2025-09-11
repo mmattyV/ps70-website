a9947dcf83b37e691dcfd051d6356e5758e00e4f
Matthew Vu <mvu@college.harvard.edu>
2025-09-11 13:55:25 -0400

    feat: configured nextjs app for github pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  basePath: '/ps70-website',
+  assetPrefix: '/ps70-website/',
+  distDir: 'out',
+  skipTrailingSlashRedirect: true,
 };
 
 export default nextConfig;
